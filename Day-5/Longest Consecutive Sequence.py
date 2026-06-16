eab4e2de8d219e461c14cf7f9ebab429c7546553
Naol Gonfa <naolgonfa449@gmail.com>
2026-06-16 16:47:31 +0300

    Day Five solution

---
+++ Day-5/Longest Consecutive Sequence.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/submissions/2035203848/
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                length = 1
+
+                while num + length in num_set:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
