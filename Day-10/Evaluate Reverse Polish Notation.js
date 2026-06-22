0da7109597dc873c9d08768d4dcb690f209872c2
Naol Gonfa <naolgonfa449@gmail.com>
2026-06-22 20:00:31 +0300

    Day 10 Task is compeleted

---
+++ Day-10/Evaluate Reverse Polish Notation.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/2042442748/
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    const stack = [];
+
+    for (let token of tokens) {
+        if (token === "+" || token === "-" || token === "*" || token === "/") {
+            const b = stack.pop();
+            const a = stack.pop();
+
+            if (token === "+") {
+                stack.push(a + b);
+            } else if (token === "-") {
+                stack.push(a - b);
+            } else if (token === "*") {
+                stack.push(a * b);
+            } else {
+                stack.push(Math.trunc(a / b));
+            }
+        } else {
+            stack.push(Number(token));
+        }
+    }
+
+    return stack[0];
+};
