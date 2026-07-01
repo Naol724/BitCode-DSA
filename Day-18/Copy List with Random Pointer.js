a343a730eb2b6791a744511e0c66a8e29bce5d96
Naol Gonfa <naolgonfa449@gmail.com>
2026-07-01 13:23:20 +0300

    Day 18 task solution

---
+++ Day-18/Copy List with Random Pointer.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/copy-list-with-random-pointer/submissions/2052221467/
+/**
+ * // Definition for a _Node.
+ * function _Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {_Node} head
+ * @return {_Node}
+ */
+var copyRandomList = function (head) {
+    if (!head) return null;
+
+    const map = new Map();
+
+    let curr = head;
+
+    while (curr) {
+        map.set(curr, new _Node(curr.val));
+        curr = curr.next;
+    }
+
+    curr = head;
+
+    while (curr) {
+        map.get(curr).next = curr.next ? map.get(curr.next) : null;
+        map.get(curr).random = curr.random ? map.get(curr.random) : null;
+        curr = curr.next;
+    }
+
+    return map.get(head);
+};
